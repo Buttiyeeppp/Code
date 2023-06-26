fe58f1f76d7c36438469b6703d7e8e95a5063507
xxzx <buttiyeeppp@outlook.com>
2023-06-26 19:18:13 +0800

    Luogu/P4396

--- Luogu/P4396.cpp
+++ Luogu/P4396.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define clo clock()/CLOCKS_PER_SEC
-//#define endl '\n'
+#define endl '\n'
 using ll=long long;
 using PII=pair<int,int>;
 const int N=1e5+10;
+const int B=500;
 bool mem1;
 int n,m,a[N],ap[N],cnt1[N],cnt2[N],blo[N];
 PII ans[N];
@@ -27,30 +28,25 @@ void del(int x) {
 }
 bool mem2;
 int main() {
-	freopen("P4396_1.in","r",stdin);
 	ios::sync_with_stdio(false), cin.tie(0);
 	
 	cin>>n>>m;
-	int B=1.*n/sqrt(m)+1, V=0;
-	cout<<B<<endl;
-	for(int i=1;i<=n;i++) cin>>a[i], blo[i]=((i<=B)? 0:blo[i-B]+1), V=max(V,a[i]);
+	for(int i=1;i<N;i++) blo[i]=(i<=B? 0:blo[i-B]+1);
+	for(int i=1;i<=n;i++) cin>>a[i];
 	for(int i=1;i<=m;i++) cin>>q[i].l>>q[i].r>>q[i].a>>q[i].b, q[i].op=i;
 	sort(q+1,q+m+1);
-	B=sqrt(V);
-	for(int i=1;i<=V;i++) blo[i]=((i<=B)? 0:blo[i-B]+1);
 	int l=1, r=0;
 	for(int i=1;i<=m;i++) {
 		while(l>q[i].l) add(--l);
 		while(r<q[i].r) add(++r);
 		while(l<q[i].l) del(l++);
 		while(r>q[i].r) del(r--);
-	cout<<"Here"<<endl;
 		int bl=blo[q[i].a], br=blo[q[i].b], ans1=0, ans2=0;
 		if(bl==br) {
 			for(int j=q[i].a;j<=q[i].b;j++) if(ap[j]) ans1+=ap[j], ans2++;
 		}
 		else {
-			for(int j=bl+1;j<br;i++) ans1+=cnt1[j], ans2+=cnt2[j];
+			for(int j=bl+1;j<br;j++) ans1+=cnt1[j], ans2+=cnt2[j];
 			for(int j=q[i].a;j<=(bl+1)*B;j++) if(ap[j]) ans1+=ap[j], ans2++;
 			for(int j=br*B+1;j<=q[i].b;j++) if(ap[j]) ans1+=ap[j], ans2++;
 		}
@@ -62,4 +58,3 @@ int main() {
 	cerr<<"Memory: "<<abs(&mem1-&mem2)/1024./1024.<<endl;
 	return 0;
 }
-
