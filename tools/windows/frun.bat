1120a487bf5ccfbf8feddbbdde4261791ddcce06
Buttiyeeppp <buttiyeeppp@outlook.com>
2024-05-11 14:22:16 +0800

    Update_mac

--- tools/windows/frun.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ %1.cpp -o %1 -std=c++14 -O2 -Wall -Wextra -Wl,--stack=512000000 -Dxxzx
-%1.exe <"%1%2".in >"%1%2".txt
-fc /w "%1%2".ans "%1%2".txt
--- tools/windows/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-g++ %1.cpp -o %1 -std=c++14 -O2 -Wall -Wextra -Wl,--stack=512000000 -Dxxzx
-%1.exe
